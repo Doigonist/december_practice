1026fbd7b537e989be4ae7097b213f37384954f2
Doigonist <doigonist@gmail.com>
2024-12-04 09:54:00 +0200

    updated css

--- php/adminpage.php
+++ php/adminpage.php
@@ -7,73 +7,50 @@ $res = $link -> query("SELECT * FROM `partners_import`");
 $res_products = $link -> query("SELECT * FROM `partner_products_import`");
 $res_products = $res_products -> fetch_all(MYSQLI_ASSOC);
 $res = $res -> fetch_all(MYSQLI_ASSOC);
-$res_skidka = $link -> query("SELECT * FROM `partner_products_import`");
-$res_skidka = $res_skidka -> fetch_all(MYSQLI_ASSOC);
-$ready_to_go = true;
+// $res_skidka = $link -> query("SELECT * FROM `partner_products_import`");
+// $res_skidka = $res_skidka -> fetch_all(MYSQLI_ASSOC);
+// $ready_to_go = true;
 }elseif($_SESSION['auth'] == 'user' ){
-    $email = $_SESSION['login'];
-    print_r($_SESSION['auth']);
-    $res = $link -> query("SELECT * FROM `partners_import` WHERE `email_partners` = '$email'");
-    $res = $res -> fetch_all(MYSQLI_ASSOC);
-    $id_partnera = $res['id_partners'];
-    $res_skidka = $link -> query("SELECT * FROM `partner_products_import`");
-    $res_skidka = $res_skidka -> fetch_all(MYSQLI_ASSOC);
-    $ready_to_go = true;
+    header('Location: userpage.php');
 }else{
-    echo 'Авторизации нет!';
+    header('Location: index.php');
 }
 ?>
-<form action="auth-handler.php" method="POST">
-    <label for="login">Введите email</label>
-<input type="text" name="login" id="">
-<label for="password">Введите ИНН</label>
-<input type="password" name="password" id="">
-<input type="submit" value="Войти">
-</form>
-<form action="logout.php">
-    <input type="submit" value="Выйти из аккаунта">
-</form>
-<table border="1px">
-<tr>
-    <th>id Партнера</th>
-    <th>Тип партнера</th>
-    <th>Название партнера</th>
-    <th>Директор партнера</th>
-    <th>email Партнера</th>
-    <th>телефон партнера</th>
-    <th>адресс партнера</th>
-    <th>инн партнера</th>
-    <th>рейтинг партнера</th>
-    <th>скидка для партнера</th>
-</tr>
-<?php
-$res_skidka = $link -> query("SELECT * FROM `partner_products_import`");
-$res_skidka = $res_skidka -> fetch_all(MYSQLI_ASSOC);
-if($ready_to_go){
-for($i = 0 ; $i < count($res); $i++){
-    if($res_skidka[$i]['product_count_partner_products'] < 10000){
-        $skidka = 0 . "%";
-    }elseif($res_skidka[$i]['product_count_partner_products'] > 10000 and $res_skida[$i]['product_count_partner_products'] < 50000){
-        $skidka = 5 . "%";
-    }elseif($res_skidka[$i]['product_count_partner_products'] > 50000 and $res_skida[$i]['product_count_partner_products'] < 300000){
-        $skidka = 10 . "%"; 
-    }elseif($res_skidka[$i]['product_count_partner_products'] > 300000){
-        $skidka = 15 . "%"; 
-    }
-    echo '<tr>';
-    echo "<td>" . $res[$i]['id_partners'] . '</td>';
-    echo "<td>" . $res[$i]['type_partner'] . '</td>';
-    echo "<td>" . $res[$i]['name_partners'] . '</td>';
-    echo "<td>" . $res[$i]['director_partners'] . '</td>';
-    echo "<td>" . $res[$i]['email_partners'] . '</td>';
-    echo "<td>" . $res[$i]['tel_partners'] . '</td>';
-    echo "<td>" . $res[$i]['address_partners'] . '</td>';
-    echo "<td>" . $res[$i]['INN_partners'] . '</td>';
-    echo "<td>" . $res[$i]['rating_partners'] . '</td>';
-    echo "<td>" . $skidka ."</td>";
-
-}
-}
-?>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="styles.css">
+		<title>Document</title>
+	</head>
+	<body>
+		<button>
+			<a href="logout.php">Выйти</a>
+        </button>
+		<div class="wrapper">
+            <?php
+            for($i = 0; $i < count($res); $i++) {
+                $type_partner = $res[$i]['type_partner'];
+                $name_partner = $res[$i]['name_partners'];
+                $director_partner = $res[$i]['director_partners'];
+                $tel_partner = $res[$i]['tel_partners'];
+                $rating_partners = $res[$i]['rating_partners'];
+                echo '<div class="wrapper-main">';
+				echo '<div class="wrapper-main-content-left">';
+				echo "<span>$type_partner | $name_partner</span>";
+				echo "<p>$director_partner</p>";
+				echo "<p>$tel_partner</p>";
+				echo "<p>Рейтинг: $rating_partners</p>";
+				echo '</div>';
+				echo '<div class="wrapper-main-content-right">';
+                echo '<span>0%</span>';
+        
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
+		</div>
+	</body>
+</html>
 
