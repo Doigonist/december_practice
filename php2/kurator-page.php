1ac3392297019dd3f7a92a29a05d1c3984c37eb6
Doigonist <doigonist@gmail.com>
2024-12-06 12:15:08 +0200

    updated php

--- php2/kurator-page.php
+++ php2/kurator-page.php
@@ -1,3 +1,7 @@
 <?php
-
+require_once('link.php');
+$id_kurator = $_SESSION['id'];
+$res = $link -> query("SELECT * FROM `student` WHERE `student-group` = '$id_kurator'");
+$res = $res -> fetch_all(MYSQLI_ASSOC);
+print_r($res);
 ?>
