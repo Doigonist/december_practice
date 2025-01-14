802ebd1f703051f571651e762feaeb272de469ad
Doigonist <doigonist@gmail.com>
2025-01-14 14:28:15 +0200

    updated january_php

--- january_php/1/index.php
+++ january_php/1/index.php
@@ -62,3 +62,51 @@ include_once("link.php");
 //         $this->salary = $this->salary * 2;
 //     }
 // }
+// class Student{
+//     public $name;
+//     public $course;
+//     public function TransferToNextCourse(){
+//         if ($this->isCourseCorrect($this->course)){
+//             $this->course = $this->course + 1;
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+//     private function isCourseCorrect($cc){
+//         return $cc < 5;
+//     }
+// }
+// $student = new Student;
+// $student->name='Danya';
+// $student->course = 1;
+// echo $student->course . '<br>';
+// $student->TransferToNextCourse();
+// echo $student->course;
+class Employee{
+    private $surname;
+    private $name;
+    private $salary;
+    public function __construct($name,$surname,$salary){
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->salary = $salary;
+    }
+    public function getSurname(){
+        return $this->surname;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getSalary(){
+        return $this->salary;
+    }
+    public function setSalary($sal){
+        $this->salary = $sal; 
+    }
+}
+$employee1 = new Employee('eric','Bobik',1000);
+$employee2 = new Employee('kyle','Nebobik',2000);
+$employee1 -> setSalary(25000);
+echo $employee1 -> getSalary();
